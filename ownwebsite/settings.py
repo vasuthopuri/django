33c9fececb98c6a7124f0bd3e0c2a87dd8f2aa0a
vasu thopuri <vasuthopuri333@gmail.com>
2019-06-17 22:54:31 +0530

    push to heroku

--- ownwebsite/settings.py
+++ ownwebsite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '@o27=8)$ql$gmoe*3b$^yr#^t!i#$3f&3be$)itm=16w@===sb'
 # SECURITY WARNING: don't run with debug turned on in production!
 ENVIRONMENT = 'production'
 DEBUG = True
-ALLOWED_HOSTS = ["vasu.thopuri.herokuapp.com"]
+ALLOWED_HOSTS = ['vasu.thopuri.herokuapp.com']
 
 # Application definitionpip
 
